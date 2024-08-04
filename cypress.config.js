df00309298651858894a3d61d7ba9fb2c474c40a
targino <contatobrunatargino@gmail.com>
2024-08-04 08:55:20 -0300

    raw: Adicionado baseUrl.

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.saucedemo.com/v1/',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
