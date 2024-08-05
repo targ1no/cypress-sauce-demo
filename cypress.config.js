a23978a5e13b314d6f1423b743d82f1f614ed5ea
targino <contatobrunatargino@gmail.com>
2024-08-04 23:19:23 -0300

    raw: Ajustando para BDD.

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,10 @@
 const { defineConfig } = require('cypress');
-const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com/v1/',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-    },
-    setupNodeEvents(on, config) {
-      on('file:preprocessor', cucumber());
     },
     reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
